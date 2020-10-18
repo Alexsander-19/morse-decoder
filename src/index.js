cbbb2ed8a88b7ce9d3aef968702d7eb3da300d03
Alexander Granovski <alexxgranovski@gmail.com>
2020-10-18 16:54:36 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const getSymbols = (str) => {
+        const result = [];
+        for (let i = 0; i < str.length; i += 10) {
+            result.push(str.slice(i, i + 10));
+        }
+        return result;
+    }
+    const symbols = getSymbols(expr);
+    const morseSymbols = symbols.map((s) => {
+        if (s === '**********') {
+            return s;
+        }
+        let result = '';
+        for(let i = 0; i < s.length; i += 2) {
+            const m = s.slice(i, i + 2);
+            if (m === '10') {
+                result = `${result}.`;
+            } else if (m === '11') {
+                result = `${result}-`;
+            }
+        }
+        return result;
+    })
+    return morseSymbols.reduce((acc, m) => {
+        if (m === '**********') {
+            return [...acc, ' '];
+        }
+        return [...acc, MORSE_TABLE[m]]
+    },[]).join('');
 }
 
 module.exports = {
